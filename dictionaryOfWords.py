180b5352b62e1e116ea29af3f4040d85d8ff3908
castle <castlesmadeofcode@gmail.com>
2020-04-14 09:53:53 -0500

    completed Practice: Dictionary of Words

--- dictionaryOfWords.py
+++ dictionaryOfWords.py
@@ -1,23 +1,34 @@
-"""
-Create a dictionary with key value pairs to
-represent words (key) and its definition (value)
-"""
+# """
+# Create a dictionary with key value pairs to
+# represent words (key) and its definition (value)
+# """
 word_definitions = dict()
 
-"""
-Add several more words and their definitions
-   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
-"""
+# """
+# Add several more words and their definitions
+#    Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+# """
 
-"""
-Use square bracket lookup to get the definition of two
-words and output them to the console with `print()`
-"""
+word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+word_definitions["Definition"] = "the act of defining, or of making something definite, distinct, or clear"
+word_definitions["Several"] = "more than two but not many."
 
 
-"""
-Loop over the dictionary to get the following output:
-    The definition of [WORD] is [DEFINITION]
-    The definition of [WORD] is [DEFINITION]
-    The definition of [WORD] is [DEFINITION]
-"""
+# """
+# Use square bracket lookup to get the definition of two
+# words and output them to the console with `print()`
+# """
+# print(word_definitions["Several"])
+# print(word_definitions["Definition"])
+
+
+
+# """
+# Loop over the dictionary to get the following output:
+#     The definition of [WORD] is [DEFINITION]
+#     The definition of [WORD] is [DEFINITION]
+#     The definition of [WORD] is [DEFINITION]
+# """
+
+for (key, value) in word_definitions.items():
+    print(f"The definition of {key} is {value}")
