cb68e998462a0a1a1073134d2eaf909d14adb1d7
castle <castlesmadeofcode@gmail.com>
2020-04-14 09:44:42 -0500

    renamed file

---
+++ dictionaryOfWords.py
@@ -0,0 +1,23 @@
+"""
+Create a dictionary with key value pairs to
+represent words (key) and its definition (value)
+"""
+word_definitions = dict()
+
+"""
+Add several more words and their definitions
+   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+"""
+
+"""
+Use square bracket lookup to get the definition of two
+words and output them to the console with `print()`
+"""
+
+
+"""
+Loop over the dictionary to get the following output:
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+"""
